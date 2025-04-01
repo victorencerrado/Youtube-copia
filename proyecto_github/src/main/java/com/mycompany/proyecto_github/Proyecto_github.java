90700bf86b13568f4894345bfe4c5d1fc4224872
Danterm2003 <danterm2003@gmail.com>
2025-04-01 17:45:22 -0600

    Update Proyecto_github.java
    agregado conexion a db class

--- proyecto_github/src/main/java/com/mycompany/proyecto_github/Proyecto_github.java
+++ proyecto_github/src/main/java/com/mycompany/proyecto_github/Proyecto_github.java
@@ -1,16 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.proyecto_github;
 
-/**
- *
- * @author victor
- */
 public class Proyecto_github {
      public static void main(String[] args) {
         System.out.println("Ejecutando Proyecto_github...");
+
+          db_class db = new db_class();
+          db.conectar();
         
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
